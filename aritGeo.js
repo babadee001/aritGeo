01b193ef8fc1d882e063dbc3f3a24ea34965bad6
babadee001 <temidayooyedele300@gmail.com>
2017-07-31 13:32:03 +0100

    modified function for proper export

--- aritGeo.js
+++ aritGeo.js
@@ -1,22 +1,24 @@
-function aritGeo(arr){
-  let firstTerm = arr[0];
-  let secondTerm = arr[1];
-  let lastTerm = arr[arr.length-1];
-  let commonDifference = secondTerm - firstTerm;
-  let apNthTerm = firstTerm + (arr.length-1) * commonDifference; // a + (n-1)d
-  let commonRatio = secondTerm/firstTerm;
-  let gpNthTerm = firstTerm * (Math.pow(commonRatio,arr.length-1)); //ar**n-1
-  if (arr.length === 0){
-    return 0;
+module.exports = {
+  aritGeo: (arr) => {
+    let firstTerm = arr[0];
+    let secondTerm = arr[1];
+    let lastTerm = arr[arr.length-1];
+    let commonDifference = secondTerm - firstTerm;
+    let apNthTerm = firstTerm + (arr.length-1) * commonDifference; // a + (n-1)d
+    let commonRatio = secondTerm/firstTerm;
+    let gpNthTerm = firstTerm * (Math.pow(commonRatio,arr.length-1)); //ar**n-1
+    if (arr.length === 0){
+      return 0;
+   }
+    if ( apNthTerm === lastTerm){
+     return "Arithmetic"; //last term always equals nth term 
+    }
+   else if(lastTerm === gpNthTerm){
+      return "Geometric"
+    }
+    else {
+      return -1;
+    }
   }
-	if ( apNthTerm === lastTerm){
-		return "Arithmetic"; //last term always equals nth term 
-	}
-	else if(lastTerm === gpNthTerm){
-	  return "Geometric"
-	}
-	else {
-	  return -1;
-	}
 }
 //console.log(aritGeo([1,2,3,22]));
